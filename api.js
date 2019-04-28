66e4ee75359360f1182f7424d6e6754fbcbf97cd
Jonas Galvez <jonasgalvez@gmail.com>
2019-04-28 04:25:52 -0300

    add json support

--- api.js
+++ api.js
@@ -1,8 +1,15 @@
+import { json } from 'body-parser'
+
 export default [
   {
     path: '/api/ping',
     handler(req, res, next) {
       res.end(`API pong at ${new Date().getTime()}`)
     }
+  },
+  json(),
+  (req, res, next) => {
+    res.json = (obj) => res.write(JSON.stringify(obj))
+    next()
   }
 ]
